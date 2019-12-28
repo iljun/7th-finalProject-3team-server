649320e1b9324ee6f13eeafacbb02db4747174d2
iljun <sumusb@naver.com>
2019-12-29 00:09:26 +0900

    add properties encryption

---
+++ src/test/java/com/example/demo/utils/EncryptionTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.utils;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EncryptionTest {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void 암호화() {
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(System.getProperty("encryptionKey"));
+
+        String enc = pbeEnc.encrypt("123");
+        log.info("enc = " + enc);
+    }
+}

6e1347b37540dc95b581f3f7b2e31e3e57774462
iljun <sumusb@naver.com>
2020-01-04 15:02:27 +0900

    add redis configuration

--- src/main/java/com/depromeet/watni/Application.java
+++ src/main/java/com/depromeet/watni/Application.java
@@ -1,9 +1,14 @@
 package com.depromeet.watni;
 
+import com.depromeet.watni.config.RedisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		RedisProperties.class
+})
 public class Application {
 
 	public static void main(String[] args) {
